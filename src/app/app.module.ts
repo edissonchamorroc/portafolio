e44c8ed5d16ffb2b962c37c9ed5e867f1e8c0b99
edissonchamorroc <john.chamorro@udea.edu.co>
2023-09-13 15:32:04 -0500

    se agrega navbar y robot3d

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { Robot3dComponent } from './shared/components/robot3d/robot3d.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    Robot3dComponent
   ],
   imports: [
     BrowserModule,
