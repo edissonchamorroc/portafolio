0578e15393af0f3c4de2b456ad0d97d7e2211764
edissonchamorroc <john.chamorro@udea.edu.co>
2025-12-04 17:36:12 -0500

    Changing spanish text to english in home page

--- src/app/article/pages/home-page/home-page.component.ts
+++ src/app/article/pages/home-page/home-page.component.ts
@@ -9,8 +9,8 @@ import { Descripcion } from '../../interfaces/descripcion.interface';
 export class HomePageComponent {
 
   public descripcion: Descripcion = {
-    nombre: 'Edisson Chamorro',
-    profesion: 'Desarrollador de Software (Spring/Angular/SerenityBDD)',
-    sobreMi: 'Soy un desarrollador de Software con experiencia en diseño, creación y mantenimiento de aplicaciones web. Me he desempeñado como desarrollador en proyectos donde he adquirido habilidades en análisis de calidad de productos de software a nivel de pruebas funcionales, desarrollo de soluciones para vulnerabilidades y soporte en diferentes proyectos. Además, he trabajado en proyectos personales con el fin de mejorar y emplear las mejores prácticas de desarrollo buscando tener código limpio. Mi enfoque está en la entrega de soluciones eficientes y escalables, con habilidades para trabajar en equipo y adaptarme a nuevas tecnologías y desafíos.'
+    name: 'Edisson Chamorro',
+    profession: 'Software developer (.Net/Spring/Angular/SerenityBDD)',
+    aboutMe: 'Edisson is a Software Developer and QA Engineer with solid experience in designing, building, and assuring the quality of services and applications. Throughout my career, I have worked on projects that strengthened my ability to guarantee product reliability, performance, and scalability. I have designed and implemented APIs and backend services to meet business requirements, ensuring clean architecture, maintainability, and efficiency. I combine development and testing expertise to prevent defects early in the lifecycle, implementing best practices such as automated testing, continuous integration, and quality gates. My experience includes functional testing, end-to-end automation, API testing, load testing, and quality assurance strategies aligned with industry standards.I am always looking to learn and apply modern technologies, architectures, and testing methodologies to deliver robust, efficient, and user-centered solutions. My goal is to contribute to teams by building software that not only works—but works reliably, securely, and at scale.'
   };
 }
