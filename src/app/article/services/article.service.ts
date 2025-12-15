cd8cd64cdf25774c6e628762cf7e7b1a31846c18
edissonchamorroc <john.chamorro@udea.edu.co>
2025-12-15 17:36:41 -0500

    Translate to english the experiences

--- src/app/article/services/article.service.ts
+++ src/app/article/services/article.service.ts
@@ -11,33 +11,31 @@ export class ArticleService {
     private myVariableSubject = new BehaviorSubject<boolean>(false);
 
     private works: work[] = [
+         {
+            id: 'cobranza-digital',
+            name: 'Cobranza digital',
+            desc: 'I had been working in Portal de Soluciones web',
+            stack: 'Java/Azure/Bizagi/VSCode/JUnit/Selenium/Serenity',
+            plataformas: 'Windows/macOS/Linux/iOS/Android'
+        },
         {
             id: 'coe-ingenieria',
             name: 'CoE Ingeniería',
-            desc: 'Trabajo en la optimización de procesos y desarrollo de talento. Apoye en la implementación de mejoras en calidad funcional y automatización por medio de la actualización de herramientas y tecnologías. Además, brindo soporte en desarrollo de automatización. Mi enfoque ha sido siempre la eficiencia y la excelencia operativa, contribuyendo al éxito del equipo y el cumplimiento de objetivos.',
+            desc: 'I have worked on process optimization initiatives aimed at improving quality across the company. I supported multiple teams in correctly implementing Java-based test automation, helping them achieve greater efficiency and reliability in their testing processes. Additionally, I developed a pipeline script to automatically detect deprecated libraries in .NET projects, contributing to better maintainability, security, and technical debt control.',
             stack: 'Java/Azure/Bizagi/VSCode/JUnit/Selenium/Serenity',
             plataformas: 'Windows/macOS/Linux/iOS/Android'
         },
         {
             id: 'vulnerabilidades',
             name: 'Vulnerabilidades',
-            desc: 'Como parte de un equipo compuesto por desarrolladores, estamos con la meta de identificar y atacar las vulnerabilidades. por medio de código, que se presenten en las diferentes Apis o Aplicativos Web que la compania gestione. Se ha logrado, hasta el momento, mejora de la seguridad de los aplicativos de portal transaccional de la compañía, reduciendo hasta un 90% las vulnerabilidades identificadas',
+            desc: 'I worked on testing and validating solutions designed to address vulnerabilities in .NET projects across the company. Through these efforts, we reduced identified vulnerabilities by 60%–90%, significantly improving the overall security posture of the applications.',
             stack: '.NET/C#/Java/Azure/JUnit/Serenity',
             plataformas: 'Windows/macOS/Linux/iOS/Android'
         },
-        // {
-        //     id: 'reposteria',
-        //     name: 'Dalia Carvajal Repostería',
-        //     desc: 'Trabajé en proyecto freelance para cliente el cual deseaba desarrollar una página web con un presupuesto limitado, por tanto planté y ejecuté la solución más optima para materealizar sus ideas.',
-        //     url: 'https://daliareposteriapasteleria.github.io/paginaweb/',
-        //     stack: 'Angular_V16/JavaScript/CSS',
-        //     plataformas: 'Windows/macOS/Linux/iOS/Android'
-
-        // },
         {
             id: 'seguros',
             name: 'Seguros y asistencias',
-            desc: 'Apoyé en el equipo de Tuya Web, con la construcción de nuevos componentes en la página de Seguros y Asistencias. La finalidad fue realizar componentes reutilizables por otros equipos sin necesidad de que codifiquen y para ello empleamos Drupal. Además apoyé en las pruebas unitarias y funcionales. El resultado fué un aumento significativo de 100.000 visitas mensuales en promedio.',
+            desc: 'I supported the team by developing Drupal components for the Seguros y Asistencias page. Additionally, I created automated tests to validate all user paths on the page. As a result of these improvements, we enhanced the user experience and increased interactions on the page by approximately 100,000 interactions per month.',
             url: 'https://www.tuya.com.co/asistencias-y-seguros',
             stack: 'Drupal/PHP/JavaScript/Sass/Java/JUnit/Serenity',
             plataformas: 'Windows/macOS/Linux/iOS/Android'
@@ -46,17 +44,17 @@ export class ArticleService {
         {
             id: 'sostenibilidad',
             name: 'Sostenibilidad',
-            desc: 'Colaboré con un equipo compuesto por UI,UX, UX Writer y Desarrolladores con el fin de construir totalmente la página Sostenibilidad. Estuve también en la parte de pruebas unitarias y funcionales. Se logró aumentar las visitas en la página web y además una reducción de la tasa de rebote del sitio web de 5 a 3 minutos, generando una mayor interacción y cohesión con los clientes por medio del contenido web.',
+            desc: 'I collaborated with a multidisciplinary team (UI, UX, UX Writer, and Developers) to fully build the Sustainability page. I contributed to unit and functional testing, improving site quality and user experience. As a result, website traffic increased and the bounce rate was reduced, leading to higher user engagement.',
             url: 'https://www.tuya.com.co/responsabilidad-social',
             stack: 'Drupal/PHP/JavaScript/Sass/JUnit/Serenity',
             plataformas: 'Windows/macOS/Linux/iOS/Android'
         },
-        
+
         {
             id: 'Contabilidad',
             name: 'Contabilidad',
             img: 'https://user-images.githubusercontent.com/71468355/219412819-6746f050-21be-41e7-b855-b7ad61c9ab3b.png',
-            desc: 'Proyecto personal en el cual se construyó una aplicación web monolítica buscando gestionar de manera básica la contabilidad de una empresa. En este proyecto se pretende mostrar el buen uso de los principios de código limpio. Se realizarón pruebas unitarias y de integración',
+            desc: 'Developed a monolithic web application to manage basic company accounting. The project demonstrates the application of clean code principles and includes unit and integration testing to ensure reliability and maintainability.',
             url: 'https://github.com/edissonchamorroc/Contabilidad-basica-empresarial',
             stack: 'Spring Web/Spring JPA/Spring Security/Postgre',
             plataformas: 'Windows/macOS/Linux'
@@ -65,7 +63,7 @@ export class ArticleService {
             id: 'sesiones',
             name: 'Sesiones concurrentes',
             img: 'https://user-images.githubusercontent.com/71468355/224379055-d0cb1248-9b59-4521-a71d-76450a6a9b9a.png',
-            desc: 'Actualmente muchas aplicaciones, como los portales transaccionales, presentan vulnerabilidad de multiple sesiones generadas a través de diferentes dispositivos como laptos, celular, etc. Es por ello que se propone como solución la construcción de una API Rest, la cual se encargue de manejar las sesiones concurrentes.',
+            desc: 'Many applications, such as transactional portals, are vulnerable to multiple concurrent sessions across different devices (laptops, mobile phones, etc.). To address this, I proposed and designed a REST API to manage and control concurrent sessions, improving security and user session management.',
             url: 'https://github.com/edissonchamorroc/RestApi-sesiones',
             stack: 'Spring boot 2.7.9/Spring web/Spring Data/postgre/JUnit 5/Mockito',
             plataformas: 'Windows/macOS/Linux'
@@ -74,7 +72,7 @@ export class ArticleService {
             id: 'storeimg',
             name: 'Store image',
             img: 'https://user-images.githubusercontent.com/71468355/217281443-2414460e-7787-4976-8d6a-104efa19019c.png',
-            desc: 'El proyecto consiste de un apartado web en donde se pueden subir imagenes a través de la funcionalidad drag and drop, o a través del sistema de buscar en el sistema de archivos, con el fin de almacenar dicho fichero en base de datos.',
+            desc: 'The project consists of a web module that allows users to upload images either via drag-and-drop or by browsing the file system. Uploaded files are then stored in a database, providing a simple and user-friendly file management solution.',
             url: 'https://github.com/edissonchamorroc/StorageImage-MVC',
             stack: 'Spring boot 2.7.8/Spring web/Spring Data/postgre',
             plataformas: 'Windows/macOS/Linux'
@@ -82,7 +80,7 @@ export class ArticleService {
         {
             id: 'pacman',
             name: 'PACMAN',
-            desc: 'El proyecto consiste en simular el video juego pacman, dando vida a algunas de sus funcionalidades a manera de práctica.',
+            desc: 'The project is a Pac-Man game simulation, implementing key functionalities as a practice exercise to improve programming and game development skills.',
             url: 'https://github.com/edissonchamorroc/PACMAN_',
             stack: 'C++/QTGraphics',
             plataformas: 'Windows/macOS/Linux'
@@ -90,18 +88,19 @@ export class ArticleService {
         {
             id: 'theory',
             name: 'Theory B',
-            desc: 'El video juego consiste en Mr Traveller, el cual va atravesando una serie de obstaculos hasta llegar a la meta. La razon por la que el Mr Traveller está en constante huida es debido a que el tiempo se le va a acabando y para poder sobrevivir deberá llegar a la meta a tiempo. En cuanto la manera de defenderse de los enemigos u obstaculos, contará con la funcionalidad de lanzar bombas.',
+            desc: 'The project is a game called Mr. Traveller, where the player navigates through a series of obstacles to reach the goal. The character is under time pressure, and to survive, they must reach the finish line before time runs out. To overcome enemies and obstacles, the player can use the bomb-throwing mechanic, adding strategy and engagement to the gameplay.',
             url: 'https://github.com/edissonchamorroc/Theory_B',
             stack: 'C++/QTGraphics',
             plataformas: 'Windows/macOS/Linux'
         },
-       
+
     ]
 
     private experiences: experience[] = [
-        { year: '10/2023 - Actualmente', title: 'Tuya CoE Ingeniería' },
-        { year: '01/2023 - Actualmente', title: 'Tuya Vulnerabilidades' },
-        { year: '02/2022 - 01/2023', title: 'Tuya Web' },
+        { year: '03/2024 - Actualmente', title: 'Cobranza digital - Tuya' },
+        { year: '10/2023 - 03/2024', title: 'CoE Ingeniería - Tuya' },
+        { year: '01/2023 - 10/2023', title: 'SMT Vulnerabilidades - Tuya' },
+        { year: '02/2022 - 01/2023', title: 'Yo tengo web - Tuya' },
         { year: '10/2021 - 02/2022', title: 'Bootcamp Backend - Sophos Solutions' },
 
     ];
